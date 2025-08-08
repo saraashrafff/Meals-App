c4be9435f5132c4ee3318727ce8c2b69a16c6246
Sara <saraashraf55530@gmail.com>
2025-08-08 19:51:24 +0300

    Add splash screen

--- lib/features/home/home_screen.dart
+++ lib/features/home/home_screen.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:meals_app/core/app_colors.dart';
 
 class HomeScreen extends StatelessWidget {
-  static const routeName = '/';
+  static const routeName = '/home';
   @override
   Widget build(BuildContext context) {
     return Scaffold(
--- lib/features/onboarding/onboarding_model.dart
+++ lib/features/onboarding/onboarding_model.dart
@@ -2,7 +2,7 @@ import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:meals_app/core/app_colors.dart';
-import 'package:meals_app/features/onboarding/onboarding_item.dart';
+import 'package:meals_app/features/onboarding/onboarding.dart';
 
 class OnboardingModel extends StatefulWidget {
   int index;
