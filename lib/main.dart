d56bca23b3fe838c7d1282159fddb1ea9d7a8908
Sara <saraashraf55530@gmail.com>
2025-08-08 14:42:17 +0300

    Add localization

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,18 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:meals_app/features/onboarding/onboarding_screen.dart';
 
-void main() {
-  runApp(MealsApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await EasyLocalization.ensureInitialized();
+  runApp(
+    EasyLocalization(
+      supportedLocales: [Locale('en'), Locale('ar')],
+      path: 'assets/translations',
+      fallbackLocale: Locale('en'),
+      child: MealsApp(),
+    ),
+  );
 }
 
 class MealsApp extends StatelessWidget {
@@ -11,6 +21,9 @@ class MealsApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      localizationsDelegates: context.localizationDelegates,
+      supportedLocales: context.supportedLocales,
+      locale: context.locale,
       debugShowCheckedModeBanner: false,
       initialRoute: OnboardingScreen.routeName,
       routes: {OnboardingScreen.routeName: (context) => OnboardingScreen()},
---
+++ lib/widgets/switch_language.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SwitchLanguage extends StatelessWidget {
+  const SwitchLanguage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Switch(value: true, onChanged: (value) {});
+  }
+}
